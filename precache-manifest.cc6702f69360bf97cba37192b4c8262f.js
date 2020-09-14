3563408d83861ae31d4ff908c813cbd5b8a1ee77
netherdraker <yinyang1520@gmail.com>
2020-09-14 12:12:16 +0700

    Updates

---
+++ precache-manifest.cc6702f69360bf97cba37192b4c8262f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991218760d66acae9d22021a04706cf2",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "3a284e79ed03f2bd4320",
+    "url": "/react-covid-tracker/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "94d1d36a7a07eba71fc9",
+    "url": "/react-covid-tracker/static/css/main.8e0db5a8.chunk.css"
+  },
+  {
+    "revision": "3a284e79ed03f2bd4320",
+    "url": "/react-covid-tracker/static/js/2.421275d6.chunk.js"
+  },
+  {
+    "revision": "6788bbbeb96ac5b9fb2d2231815fe398",
+    "url": "/react-covid-tracker/static/js/2.421275d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94d1d36a7a07eba71fc9",
+    "url": "/react-covid-tracker/static/js/main.6df331eb.chunk.js"
+  },
+  {
+    "revision": "261fd4b71819be31f984",
+    "url": "/react-covid-tracker/static/js/runtime-main.803e8507.js"
+  }
+]);
